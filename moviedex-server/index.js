e52cfa5742b7c71d2a74728a5463ec9403b7d087
SamDino <samuel.cas.96@gmail.com>
2020-02-05 09:00:20 -0600

    Part 1 and 2 Finished

--- moviedex-server/index.js
+++ moviedex-server/index.js
@@ -1,12 +1,18 @@
-let express = require( 'express' );
-let bodyParser = require( 'body-parser' );
-let mongoose = require( 'mongoose' );
+let express = require('express');
+let bodyParser = require('body-parser');
+let mongoose = require('mongoose');
 let jsonParser = bodyParser.json();
-let { DATABASE_URL, PORT } = require( './config' );
+let {
+	DATABASE_URL,
+	PORT
+} = require('./config');
 
 let app = express();
+let {
+	MovieList
+} = require('./model');
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Content-Type,Authorization");
 	res.header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE");
@@ -17,49 +23,78 @@ app.use(function(req, res, next) {
 });
 
 /* Tu código va aquí */
+app.get('/api/moviedex', (req, res) => {
+	return MovieList.getAll().then((allMovies) => {
+		return res.status(200).json(allMovies);
+	});
+});
+
+app.post('/api/moviedex', jsonParser, (req, res) => {
+	let newMovie = req.body;
+	if (isBlank(newMovie.film_title) || isBlank(newMovie.year) || isBlank(newMovie.rating)) {
+		return res.status(406).send('Campos de pelicula vacios.');
+	} else {
+		MovieList.addMovie(newMovie)
+			.then((newMovie) => {
+				return res.status(201).json(newMovie);
+			})
+			.catch((error) => {
+				return res.status(400).send('DB error: ' + error);
+			});
+	}
+});
 
 let server;
 
-function runServer( port, databaseUrl ){
-	return new Promise( (resolve, reject ) => {
-		mongoose.connect( databaseUrl,  { useNewUrlParser: true, useUnifiedTopology: true  }, response => {
-			if ( response ){
-				return reject( response );
-			}
-			else{
+function runServer(port, databaseUrl) {
+	return new Promise((resolve, reject) => {
+		mongoose.connect(databaseUrl, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true
+		}, response => {
+			if (response) {
+				return reject(response);
+			} else {
 				server = app.listen(port, () => {
-					console.log( "App is running on port " + port );
-					resolve();
-				})
-				.on( 'error', err => {
-					mongoose.disconnect();
-					return reject(err);
-				})
+						console.log("App is running on port " + port);
+						resolve();
+					})
+					.on('error', err => {
+						mongoose.disconnect();
+						return reject(err);
+					})
 			}
 		});
 	});
 }
 
-function closeServer(){
+function closeServer() {
 	return mongoose.disconnect()
-		.then( () => {
-			return new Promise( (resolve, reject) => {
-				console.log( 'Closing the server' );
-				server.close( err => {
-					if ( err ){
-						return reject( err );
-					}
-					else{
+		.then(() => {
+			return new Promise((resolve, reject) => {
+				console.log('Closing the server');
+				server.close(err => {
+					if (err) {
+						return reject(err);
+					} else {
 						resolve();
 					}
 				});
 			});
 		});
 }
-runServer( PORT, DATABASE_URL );
+runServer(PORT, DATABASE_URL);
 
-module.exports = { 
-    app, 
-    runServer, 
-    closeServer 
+module.exports = {
+	app,
+	runServer,
+	closeServer
+}
+
+function isBlank(param) {
+	if (param == null || param == undefined || param == '' || param == ' ') {
+		return true;
+	} else {
+		return false;
+	}
 }
--- moviedex-server/model.js
+++ moviedex-server/model.js
@@ -1,10 +1,49 @@
-let mongoose = require( 'mongoose' );
-let uuid = require( 'uuid' );
+let mongoose = require('mongoose');
+let uuid = require('uuid');
 
 mongoose.Promise = global.Promise;
 
 /* Tu código va aquí */
+let Movie = mongoose.Schema({
+    film_ID: {
+        type: uuid()
+    },
+    film_title: {
+        type: String
+    },
+    year: {
+        type: Number
+    },
+    rating: {
+        type: Number
+    }
+});
+
+let movies = mongoose.model('moviedex', Movie);
+
+let MovieList = {
+    getAll: function () {
+        return movies.find()
+            .then((allMovies) => {
+                return allMovies;
+            })
+            .catch((error) => {
+                throw Error(error);
+            });
+    },
+    addMovie: function (newMovie) {
+        return movies.create(newMovie)
+            .then((movieAdded) => {
+                return movieAdded;
+            })
+            .catch((error) => {
+                throw Error(error);
+            });
+    }
+}
+
+
 
 module.exports = {
-    
+    MovieList
 };
